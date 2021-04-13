d50b8a6f456bc721dfb59ec62d9b98a27f093e3f
thiendangute201 <1911505310261@sv.ute.udn.vn>
2021-04-13 21:34:02 +0700

    Tao package model

--- src/Model/SanPham.java
+++ src/Model/SanPham.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package quanlynhanvien;
+package Model;
 
 /**
  *
  * @author thien
  */
-public class QuanLyNhanVien {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+public class SanPham {
     
 }
