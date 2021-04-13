d7e5a09f20ad2421398d58e6dfc10a46bfc1499c
thiendangute201 <1911505310261@sv.ute.udn.vn>
2021-04-13 21:28:39 +0700

    Hello world

--- src/quanlynhanvien/QuanLyNhanVien.java
+++ src/quanlynhanvien/QuanLyNhanVien.java
@@ -15,6 +15,7 @@ public class QuanLyNhanVien {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello world");
     }
     
 }
